e28889d61033947a5aebd5557f6d7d70ec80e1f8
Gary Black <gar2000b@yahoo.com>
2017-10-21 03:28:26 +0100

    Enhancing Main app to execute a new window using start and then within that, execute several commands.

--- src/main/java/com/onlineinteract/Main.java
+++ src/main/java/com/onlineinteract/Main.java
@@ -14,7 +14,8 @@ public class Main {
 	public static void main(String[] args) throws InterruptedException {
 		Runtime runtime = Runtime.getRuntime();
 		try {
-			Process exec = runtime.exec("notepad.exe");
+			// Process exec = runtime.exec("notepad.exe");
+			Process exec = runtime.exec("cmd /c start \"MyWindow\" cmd /c \"ping localhost & ipconfig & dir & timeout 5\"");
 			Thread.sleep(2000);
 			exec.destroy();
 		} catch (IOException e) {
--- src/main/java/com/onlineinteract/springboot/LaunchSpringbootApp.java
+++ src/main/java/com/onlineinteract/springboot/LaunchSpringbootApp.java
@@ -6,19 +6,28 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class LaunchSpringbootApp {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		Runtime runtime = Runtime.getRuntime();
 		try {
 			Process exec = runtime.exec(
 					"java -jar /work/stateful-list-service/target/stateful-list-service-0.0.1-SNAPSHOT.jar");
+			new Thread(() -> {
+				try {
+					Thread.sleep(20000);
+					exec.destroy();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}).start();
 			InputStream inputStream = exec.getInputStream();
 			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
 			String line = "";
 			while ((line = bufferedReader.readLine()) != null) {
 				System.out.println(line);
+				if (line.contains("Tomcat started on port(s): 8080"))
+					System.out.println("Application launched successfully!");
 			}
-			Thread.sleep(2000);
-			exec.destroy();
+			System.out.println("Process destroyed, exiting.");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
